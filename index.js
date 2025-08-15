095c787d3ec3e9baa479d07483ca16beafbe4e2b
K7413dS433d <khaledsaied2002@gmail.com>
2025-08-15 17:38:15 +0300

    app v1

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send("Hello, Notion Kanban!")
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
