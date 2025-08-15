3429eaa7173e751ba2494876303b1f33b96db16c
K7413dS433d <khaledsaied2002@gmail.com>
2025-08-15 17:39:44 +0300

    test: test working by change the cronjob time

--- src/Scheduler/jobs/kanban-reset-tasks.js
+++ src/Scheduler/jobs/kanban-reset-tasks.js
@@ -2,7 +2,8 @@ import { CronJob } from 'cron';
 import { resetTasks } from '../tasks/reset-tasks.js';
 
 export const job = new CronJob(
-    '0 0 * * *', // cronTime
+    // '0 0 * * *', // cronTime
+    '* * * * *', // cronTime
     resetTasks, // onTick
     null, // onComplete
     true, // start
